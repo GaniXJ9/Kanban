9ea034bdb69ec80a5147e333f5a23c27036aefa4
ganiv847@gmail.com <ganisergaliev29@gmail.com>
2025-07-14 18:06:16 +0500

    fix: header

--- src/widgets/Header/InputBlock/InputBlock.tsx
+++ src/widgets/Header/InputBlock/InputBlock.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useStore from "../../../app/store";
 import CreateBlock from "./CreateBlock";
+import Search from "../../../shared/icons/SearchIcon";
 
 const InputBlock = () => {
   const [showCreateBoardBlock, setShowCreateBoardBlock] =
@@ -17,12 +18,18 @@ const InputBlock = () => {
 
   return (
     <div className="relative w-1/2 h-9 flex items-center gap-5 ">
-      <input
-        type="text"
-        className={`border w-full h-full text-white px-5 rounded-md lg:hover:cursor-pointer ${
-          theme === "light" ? "border-white " : "border-[#838383] "
-        }`}
-      />
+      <div className="relative w-full">
+        <span className="absolute text-white top-2 left-2">
+          <Search />
+        </span>
+        <input
+          type="text"
+          placeholder="Search"
+          className={`border w-full  text-white p-1 px-8 rounded-md lg:hover:cursor-pointer outline-none ${
+            theme === "light" ? "border-white " : "border-[#838383] "
+          }`}
+        />
+      </div>
       <button
         onClick={toggleCreateBoardBlock}
         className={`text-white border  px-5 h-full rounded-md lg:hover:cursor-pointer ${
